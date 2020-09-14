9914467fab5ccf5844c9b648fc41100551a3928a
Khoi Nguyen <nguyenkhoi2190@gmail.com>
2020-09-14 23:10:26 +0800

    Create FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Khoi Nguyen on 14/9/20.
+//  Copyright © 2020 Khoi Nguyen. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
