9e4449d6b40e1efd0b32e3a9d4938816fadc0af3
Khoi Nguyen <nguyenkhoi2190@gmail.com>
2020-09-14 23:15:46 +0800

    Create FeedLoader protocol boundary

---
+++ EssentialFeed/EssentialFeed/FeedLoader.swift
@@ -0,0 +1,18 @@
+//
+//  FeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Khoi Nguyen on 14/9/20.
+//  Copyright © 2020 Khoi Nguyen. All rights reserved.
+//
+
+import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case error(Error)
+}
+
+protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult) -> Void)
+}
