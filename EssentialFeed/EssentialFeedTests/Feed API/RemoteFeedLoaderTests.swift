28d09b2771068cc3da462914fa7eb0033ee383fb
Khoi Nguyen <nguyenkhoi2190@gmail.com>
2020-10-13 16:02:40 +0800

    Extract track memory leak to share helper method

--- EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -123,13 +123,6 @@ class RemoteFeedLoaderTests: XCTestCase {
         return .failure(error)
     }
     
-    
-    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance, "Instance should have been deallocated. Potential meomory leak.", file: file, line: line)
-        }
-    }
-    
     private func expect(_ sut: RemoteFeedLoader, toCompleteWithResult expectedResult: RemoteFeedLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
         let exp = expectation(description: "Wait for load completion")
         sut.load { receivedResult in
---
+++ EssentialFeed/EssentialFeedTests/Helpers/XCTestCase+MemoryLeak.swift
@@ -0,0 +1,20 @@
+//
+//  Helpers.swift
+//  EssentialFeedTests
+//
+//  Created by Khoi Nguyen on 13/10/20.
+//  Copyright © 2020 Khoi Nguyen. All rights reserved.
+//
+
+import XCTest
+
+extension XCTestCase {
+    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential meomory leak.", file: file, line: line)
+        }
+    }
+}
+
+
+
