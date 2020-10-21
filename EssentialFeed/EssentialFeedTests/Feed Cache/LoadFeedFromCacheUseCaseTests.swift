75cfad1c745d12e80c8118d36fc3dafee16a4121
Khoi Nguyen <nguyenkhoi2190@gmail.com>
2020-10-21 14:43:18 +0800

    Save requests to delete cached feed

--- EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import XCTest
+import EssentialFeed
 
 class LocalFeedLoader {
     private let store: FeedStore
@@ -15,10 +16,18 @@ class LocalFeedLoader {
     init(store: FeedStore) {
         self.store = store
     }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
+    }
 }
 
 class FeedStore {
     var deleteCachedFeedCallCount = 0
+    
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 
 class LoadFeedFromCacheUseCaseTests: XCTestCase {
@@ -27,5 +36,24 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         _ = LocalFeedLoader(store: store)
         
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+    
+    func test_save_deletesCachedFeed() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = uniqueItem()
+        sut.save([items])
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+    
+    //MARK: - Helpers
+    
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        URL(string: "http://any-url.com")!
     }
 }
