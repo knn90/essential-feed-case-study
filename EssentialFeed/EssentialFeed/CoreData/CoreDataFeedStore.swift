98d1d1481ef71867ef51202c9ba0da493b9e96c0
Khoi Nguyen <nguyenkhoi2190@gmail.com>
2020-11-08 17:33:28 +0800

    Retrieve deliver found value on non empty cache

--- EssentialFeed/EssentialFeed/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/CoreData/CoreDataFeedStore.swift
@@ -22,11 +22,43 @@ public class CoreDataFeedStore: FeedStore {
     }
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping (InsertionCompletion)) {
-        
+        let context = self.context
+        context.perform {
+            let set = NSOrderedSet(array: feed.map {
+                let managedFeed = ManagedFeedImage(context: context)
+                managedFeed.id  = $0.id
+                managedFeed.imageDescription = $0.description
+                managedFeed.location = $0.location
+                managedFeed.url = $0.url
+                return managedFeed
+            })
+            let managedCache = ManagedCache(context: context)
+            managedCache.feed = set
+            managedCache.timestamp = timestamp
+            context.insert(managedCache)
+            try! context.save()
+            completion(nil)
+        }
     }
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        completion(.empty)
+        let context = self.context
+        context.perform {
+            let request = NSFetchRequest<ManagedCache>(entityName: "ManagedCache")
+            let caches = try! context.fetch(request)
+            if !caches.isEmpty {
+                let feed = caches[0].feed
+                    .compactMap { $0 as? ManagedFeedImage }
+                    .map { LocalFeedImage(id: $0.id,
+                                          description: $0.imageDescription,
+                                          location: $0.location,
+                                          url: $0.url) }
+                let timestamp = caches[0].timestamp
+                completion(.found(feed: feed, timestamp: timestamp))
+            } else {
+                completion(.empty)
+            }
+        }
     }
 }
 
