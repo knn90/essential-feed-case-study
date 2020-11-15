2c9255ab1694b147ef53d25bba08e126f60f5a21
Khoi Nguyen <nguyenkhoi2190@gmail.com>
2020-11-15 13:40:14 +0800

    Render loaded feed on successful load completion

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Khoi Nguyen on 15/11/20.
+//  Copyright © 2020 Khoi Nguyen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public var locationContainer = UIView()
+    public var locationLabel = UILabel()
+    public var descriptionLabel = UILabel()
+}
