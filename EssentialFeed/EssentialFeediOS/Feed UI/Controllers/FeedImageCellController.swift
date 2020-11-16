4a9517396618a86aa93f4a040db6becda81de192
Khoi Nguyen <nguyenkhoi2190@gmail.com>
2020-11-16 23:26:04 +0800

    Extract `FeedImageCell` creation/configuration and image loading logic with `FeedImageDataLoader` to a new `FeedImageCellController`

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -0,0 +1,58 @@
+//
+//  FeedImageCellController.swift
+//  EssentialFeediOS
+//
+//  Created by Khoi Nguyen on 16/11/20.
+//  Copyright © 2020 Khoi Nguyen. All rights reserved.
+//
+
+import UIKit
+import EssentialFeed
+
+final class FeedImageCellController {
+    private let model: FeedImage
+    private let imageLoader: FeedImageDataLoader
+
+    private var task: FeedImageDataLoaderTask?
+    
+    init(model: FeedImage, imageLoader: FeedImageDataLoader) {
+        self.model = model
+        self.imageLoader = imageLoader
+    }
+    
+    func view() -> FeedImageCell {
+        let cell = FeedImageCell()
+        cell.locationContainer.isHidden = (model.location == nil)
+        cell.locationLabel.text = model.location
+        cell.descriptionLabel.text = model.description
+        cell.feedImageView.image = nil
+        cell.feedImageRetryButton.isHidden = true
+        cell.imageContainer.startShimmering()
+        
+        let loadImage = { [weak self, weak cell] in
+            guard let self = self else { return }
+            
+            self.task = self.imageLoader.loadImageData(from: self.model.url) { [weak cell] result in
+                let data = try? result.get()
+                let image = data.map(UIImage.init) ?? nil
+                cell?.feedImageView.image = image
+                cell?.feedImageRetryButton.isHidden = (image != nil)
+                cell?.imageContainer.stopShimmering()
+            }
+        }
+        
+        cell.onRetry = loadImage
+        loadImage()
+        
+        return cell
+    }
+    
+    func preload() {
+        task = self.imageLoader.loadImageData(from: self.model.url) { _ in }
+    }
+
+    deinit {
+        task?.cancel()
+    }
+
+}
