0fb14f9de2fdbc8f6a72b4974ea7638b9c20f357
Khoi Nguyen <nguyenkhoi2190@gmail.com>
2020-11-21 22:48:14 +0800

    Move `FeedLoaderPresentationAdapter` to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedLoaderPresentationAdapter.swift
@@ -0,0 +1,30 @@
+//
+//  FeedLoaderPresentationAdapter.swift
+//  EssentialFeediOS
+//
+//  Created by Khoi Nguyen on 21/11/20.
+//  Copyright © 2020 Khoi Nguyen. All rights reserved.
+//
+
+import EssentialFeed
+
+final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
+    private let feedLoader: FeedLoader
+    var feedPresenter: FeedPresenter?
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    func didRequestFeedRefresh() {
+        feedPresenter?.disStartLoadingFeed()
+        feedLoader.load { [weak self] result in
+            switch result {
+            case let .success(feed):
+                self?.feedPresenter?.didFinishLoadingFeed(with: feed)
+            case let .failure(error):
+                self?.feedPresenter?.didFinishLoadingFeed(with: error)
+            }
+        }
+    }
+}
--- EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -62,24 +62,3 @@ private final class FeedViewAdapter: FeedView {
 
     }
 }
-
-private final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
-    private let feedLoader: FeedLoader
-    var feedPresenter: FeedPresenter?
-    
-    init(feedLoader: FeedLoader) {
-        self.feedLoader = feedLoader
-    }
-    
-    func didRequestFeedRefresh() {
-        feedPresenter?.disStartLoadingFeed()
-        feedLoader.load { [weak self] result in
-            switch result {
-            case let .success(feed):
-                self?.feedPresenter?.didFinishLoadingFeed(with: feed)
-            case let .failure(error):
-                self?.feedPresenter?.didFinishLoadingFeed(with: error)
-            }
-        }
-    }
-}
