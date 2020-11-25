68ae644150635ffb394e251ef49dcc953b749dd0
Khoi Nguyen <nguyenkhoi2190@gmail.com>
2020-11-25 22:42:37 +0800

    Add `fail` transformation to clarify intent

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -79,7 +79,7 @@ class FeedImagePresenterTests: XCTestCase {
     }
     
     func test_didFinishLoadingImageData_displaysRetryOnFailedImageTransformation() {
-        let (sut, view) = makeSUT(imageTransformer: { _ in nil})
+        let (sut, view) = makeSUT(imageTransformer: fail)
         let image = uniqueImage()
         let data = Data()
         
@@ -103,7 +103,11 @@ class FeedImagePresenterTests: XCTestCase {
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, view)
     }
-
+    
+    private var fail: (Data) -> Any? {
+        return { _ in nil }
+    }
+    
     private class ViewSpy: FeedImageView {
         private(set) var messages = [FeedImageViewModel]()
         
