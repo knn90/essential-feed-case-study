f2ace1c12dc640934919be8a0c02001346bb86a4
Khoi Nguyen <nguyenkhoi2190@gmail.com>
2020-11-25 15:18:49 +0800

    Move `FeedErrorViewModel` to its own file

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Khoi Nguyen on 25/11/20.
+//  Copyright © 2020 Khoi Nguyen. All rights reserved.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
