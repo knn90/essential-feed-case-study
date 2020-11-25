dea4b3df8f0182889e80d08acdc246b23c5840d8
Khoi Nguyen <nguyenkhoi2190@gmail.com>
2020-11-25 14:45:31 +0800

    Move ErrorView to its own file

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Khoi Nguyen on 25/11/20.
+//  Copyright © 2020 Khoi Nguyen. All rights reserved.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
