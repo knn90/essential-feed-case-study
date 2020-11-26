c3fda6e179b677b1459b3361bd7c926df71fe6b6
Khoi Nguyen <nguyenkhoi2190@gmail.com>
2020-11-26 23:00:35 +0800

    Add HTTPClientTask to perform cancel URL Request

--- EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -111,6 +111,14 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
         XCTAssertTrue(capturedResults.isEmpty)
     }
     
+    func test_cancelGetFromURLTask_cancelsURLRequest() {
+        let (sut, client) = makeSUT()
+        let url = anyURL()
+        let task = sut.loadImageData(from: url) { _ in }
+        
+        XCTAssertEqual(client.cancelledURLs, [url])
+    }
+    
     //MARK: - Helpers
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (RemoteFeedImageDataLoader, HTTPClientSpy) {
         let client = HTTPClientSpy()
@@ -147,9 +155,15 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
             return messages.map { $0.url }
         }
         var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
+        var cancelledURLs = [URL]()
         
-        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+        private struct Task: HTTPClientTask {
+            func cancel() {}
+        }
+        
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
             messages.append((url, completion))
+            return Task()
         }
         
         func complete(with error: Error, at index: Int = 0) {
