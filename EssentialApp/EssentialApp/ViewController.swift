fb80823870700c4668b3b4b7a792f909549f588b
knn <nguyenkhoi2190@gmail.com>
2020-12-16 23:51:52 +0800

    Create empty `EssentialApp` project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Khoi Nguyen on 16/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
