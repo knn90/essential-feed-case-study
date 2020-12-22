856edf4066604853e0f2c5de92c68cc7b6270091
knn <nguyenkhoi2190@gmail.com>
2020-12-23 00:01:31 +0800

    Record feed with failed image loading

--- EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
@@ -35,6 +35,14 @@ class FeedSnapshotTests: XCTestCase {
         record(snapshot: sut.snapshot(), name: "FEED_WITH_ERROR_MESSAGE")
     }
     
+    func test_feedWithFailedImageLoading() {
+        let sut = makeSUT()
+        
+        sut.display(feedWithFailedImageLoading())
+        
+        record(snapshot: sut.snapshot(), name: "FEED_WITH_FAILED_IMAGE_LOADING")
+    }
+    
     // MARK: - Helpers
     private func makeSUT( file: StaticString = #file, line: UInt = #line) -> FeedViewController {
         let bundle = Bundle(for: FeedViewController.self)
@@ -61,6 +69,13 @@ class FeedSnapshotTests: XCTestCase {
                 location: "Garth Pier",
                 image: UIImage.make(withColor: .green)
             )
+        ]
+    }
+    
+    private func feedWithFailedImageLoading() -> [ImageStub] {
+        return [
+            ImageStub(description: nil, location: "Cannon street, London", image: nil),
+            ImageStub(description: nil, location: "Brighton Seafront", image: nil)
         ]
     }
     
