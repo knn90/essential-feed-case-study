2c79436ed31a89b3dd20fab8183630c554e3db24
knn <nguyenkhoi2190@gmail.com>
2020-12-23 10:51:54 +0800

    Fix potential bug when cancelling requests in UITableView didEndDisplayCell method.This method invoke after calling `ReloadData`, so we'd be cancelling requests in the wrong model or crash in case the model has less items then previous model

--- EssentialApp/EssentialAppTests/Helpers/FeedViewControllerTests+Assertions.swift
+++ EssentialApp/EssentialAppTests/Helpers/FeedViewControllerTests+Assertions.swift
@@ -27,7 +27,8 @@ extension FeedUIIntegrationTests {
     }
     
     func assertThat(_ sut: FeedViewController, isRendering feed: [FeedImage], file: StaticString = #file, line: UInt = #line) {
-        
+        sut.tableView.layoutIfNeeded()
+        RunLoop.main.run(until: Date())
         guard sut.numberOfRenderedFeedImageViews() == feed.count else {
             return XCTFail("Expected \(feed.count), got \(sut.numberOfRenderedFeedImageViews()) instead", file: file, line: line)
         }
